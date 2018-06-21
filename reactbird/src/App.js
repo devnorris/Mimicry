001b8662646f6cea9d8d9325837ea6043791c1c3
Devon Norris <norris.devon@gmail.com>
2018-06-21 13:29:34 -0400

    put api-key in gitignore

--- reactbird/src/App.js
+++ reactbird/src/App.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+const API_KEY = process.env.REACT_APP_LYREBIRD_API_KEY;
+const apiConfig = {
+  headers: { Authorization: API_KEY }
+}
 
 class App extends Component {
+
+  componentDidMount() {
+
+    axios.get('https://custom.lyrebird.ai/api/v0/voices', apiConfig )
+    .then(res => {
+      console.log("res", res.data)
+    })
+    .catch(error => {
+      console.log("error", error)
+    })
+  }
+
+
   render() {
     return (
       <div className="App">
